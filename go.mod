22fcd001b2718b22b43fc6ce54da4a23f404a46b
eavesmy <eavesmy@gmail.com>
2021-02-17 21:50:59 +0900

    add ip func.

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/eavesmy/golang-lib
 
 go 1.14
+
+require (
+	github.com/go-http-utils/cookie v1.3.1
+	github.com/google/uuid v1.2.0
+	github.com/satori/go.uuid v1.2.0
+)
---
+++ net/ip.go
@@ -0,0 +1,28 @@
+package net
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func GetIntranetIp() []string {
+	ips := []string{}
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, address := range addrs {
+
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ips = append(ips, ipnet.IP.String())
+			}
+		}
+	}
+	return ips
+}
