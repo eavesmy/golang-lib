31d01ad67ee60e2517579849fa11596b328e6ddc
eavesmy <eavesmy@gmail.com>
2021-05-25 16:41:18 +0900

    add sha256withrsa

--- auth/token.go
+++ auth/token.go
@@ -1,11 +1,20 @@
+/*
+ * @Author: jjc
+ * @Date: 2021-05-25 14:00:03
+ * @LastEditTime: 2021-05-25 14:28:33
+ * @LastEditors: Please set LastEditors
+ * @Description:
+ * @FilePath: /wwwroot/root/go/pkg/mod/github.com/eavesmy/golang-lib@v0.3.0/auth/token.go
+ */
 package auth
 
 import (
-	"strings"
-	"time"
+    "encoding/base64"
+    "strings"
+    "time"
 
-	"github.com/eavesmy/golang-lib/crypto"
-	gtype "github.com/eavesmy/golang-lib/type"
+    "github.com/eavesmy/golang-lib/crypto"
+    gtype "github.com/eavesmy/golang-lib/type"
 )
 
 var KEY = "zhongnanhai"
@@ -21,59 +30,61 @@ sign: rc4(user_id,uid=user_id&t=timestamp)
 
 func GenToken(uid string) string {
 
-	t := gtype.Int642String(time.Now().Unix())
+    t := gtype.Int642String(time.Now().Unix())
 
-	sign := ""
-	sign += "uid=" + uid
-	sign += "&"
-	sign += "t=" + t
+    sign := ""
+    sign += "uid=" + uid
+    sign += "&"
+    sign += "t=" + t
+    //@jjc
+    sign = base64.StdEncoding.EncodeToString([]byte(sign))
+    sign = crypto.Rc4(sign, uid)
 
-	sign = crypto.Rc4(sign, uid)
-
-	token := uid + "|" + t + "|" + sign
-
-	return crypto.Rc4(token, KEY)
+    token := uid + "|" + t + "|" + sign
+    token = base64.StdEncoding.EncodeToString([]byte(token))
+    return crypto.Rc4(token, KEY)
 }
 
 func ParseToken(token string) (uid, t, sign string) {
 
-	token = crypto.Rc4(token, KEY)
+    token = crypto.Rc4(token, KEY)
+    temp_data, _ := base64.StdEncoding.DecodeString(token)
+    token = string(temp_data)
+    arr := strings.Split(token, "|")
 
-	arr := strings.Split(token, "|")
+    if len(arr) < 3 {
+        return
+    }
 
-	if len(arr) < 3 {
-		return
-	}
+    uid = arr[0]
+    t = arr[1]
+    sign = arr[2]
 
-	uid = arr[0]
-	t = arr[1]
-	sign = arr[2]
-
-	return
+    return
 }
 
 func VeriSign(uid, t, sign string) bool {
 
-	str := crypto.Rc4(sign, uid)
+    str := crypto.Rc4(sign, uid)
 
-	arr := strings.Split(str, "&")
-	if len(arr) < 2 {
-		return false
-	}
+    arr := strings.Split(str, "&")
+    if len(arr) < 2 {
+        return false
+    }
 
-	for _, item := range arr {
-		param := strings.Split(item, "=")
-		k := param[0]
-		v := param[1]
+    for _, item := range arr {
+        param := strings.Split(item, "=")
+        k := param[0]
+        v := param[1]
 
-		if k == "uid" && v != uid {
-			return false
-		}
+        if k == "uid" && v != uid {
+            return false
+        }
 
-		if k == "t" && v != t {
-			return false
-		}
-	}
+        if k == "t" && v != t {
+            return false
+        }
+    }
 
-	return true
+    return true
 }
---
+++ crypto/sha256withrsa.go
@@ -0,0 +1,64 @@
+package crypto
+
+func Sha256WithRsa(privateRaw string, msg string) (string, error) {
+	privateRaw = strings.Trim(privateRaw, "\n")
+	if !strings.HasPrefix(privateRaw, "-----BEGIN RSA PRIVATE KEY-----") {
+		privateRaw = fmt.Sprintf("%s\n%s\n%s", "-----BEGIN RSA PRIVATE KEY-----", privateRaw, "-----END RSA PRIVATE KEY-----")
+	}
+
+	blockPri, _ := pem.Decode([]byte(privateRaw))
+	if blockPri == nil {
+		return "", fmt.Errorf("blockPri is nil")
+	}
+
+	rsaPri, e := genPriKey(blockPri.Bytes, PKCS8)
+	if e != nil {
+		panic(e)
+	}
+
+	h := sha256.New()
+	h.Write([]byte(msg))
+	d := h.Sum(nil)
+
+	signature, err := rsa.SignPKCS1v15(rand.Reader, rsaPri, crypto.SHA256, d)
+	if err != nil {
+		return "", errorx.Wrap(err)
+	}
+	encodedSig := base64.StdEncoding.EncodeToString(signature)
+	return encodedSig, nil
+}
+
+const (
+	PKCS1 int64 = iota
+	PKCS8
+)
+
+func genPriKey(privateKey []byte, privateKeyType int64) (*rsa.PrivateKey, error) {
+	var priKey *rsa.PrivateKey
+	var err error
+	switch privateKeyType {
+	case PKCS1:
+		{
+			priKey, err = x509.ParsePKCS1PrivateKey([]byte(privateKey))
+			if err != nil {
+				return nil, err
+			}
+		}
+	case PKCS8:
+		{
+			prkI, err := x509.ParsePKCS8PrivateKey([]byte(privateKey))
+			if err != nil {
+				return nil, err
+			}
+			priKey = prkI.(*rsa.PrivateKey)
+		}
+	default:
+		{
+			return nil, fmt.Errorf("unsupport private key type")
+		}
+	}
+	return priKey, nil
+}
+————————————————
+版权声明：本文为CSDN博主「fwhezfwhez」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/fwhezfwhez/article/details/113120106
