c3f61e23d6af9c8fd52c99525d4e68b0fc87ffdd
eavesmy <eavesmy@gmail.com>
2021-05-25 17:14:04 +0900

    update

--- auth/token.go
+++ auth/token.go
@@ -9,12 +9,11 @@
 package auth
 
 import (
-    "encoding/base64"
-    "strings"
-    "time"
+	"strings"
+	"time"
 
-    "github.com/eavesmy/golang-lib/crypto"
-    gtype "github.com/eavesmy/golang-lib/type"
+	"github.com/eavesmy/golang-lib/crypto"
+	gtype "github.com/eavesmy/golang-lib/type"
 )
 
 var KEY = "zhongnanhai"
@@ -30,61 +29,61 @@ sign: rc4(user_id,uid=user_id&t=timestamp)
 
 func GenToken(uid string) string {
 
-    t := gtype.Int642String(time.Now().Unix())
+	t := gtype.Int642String(time.Now().Unix())
 
-    sign := ""
-    sign += "uid=" + uid
-    sign += "&"
-    sign += "t=" + t
-    //@jjc
-    sign = base64.StdEncoding.EncodeToString([]byte(sign))
-    sign = crypto.Rc4(sign, uid)
+	sign := ""
+	sign += "uid=" + uid
+	sign += "&"
+	sign += "t=" + t
+	//@jjc
+	// sign = base64.StdEncoding.EncodeToString([]byte(sign))
+	sign = crypto.Rc4(sign, uid)
 
-    token := uid + "|" + t + "|" + sign
-    token = base64.StdEncoding.EncodeToString([]byte(token))
-    return crypto.Rc4(token, KEY)
+	token := uid + "|" + t + "|" + sign
+	// token = base64.StdEncoding.EncodeToString([]byte(token))
+	return crypto.Rc4(token, KEY)
 }
 
 func ParseToken(token string) (uid, t, sign string) {
 
-    token = crypto.Rc4(token, KEY)
-    temp_data, _ := base64.StdEncoding.DecodeString(token)
-    token = string(temp_data)
-    arr := strings.Split(token, "|")
+	token = crypto.Rc4(token, KEY)
+	// temp_data, _ := base64.StdEncoding.DecodeString(token)
+	// token = string(temp_data)
+	arr := strings.Split(token, "|")
 
-    if len(arr) < 3 {
-        return
-    }
+	if len(arr) < 3 {
+		return
+	}
 
-    uid = arr[0]
-    t = arr[1]
-    sign = arr[2]
+	uid = arr[0]
+	t = arr[1]
+	sign = arr[2]
 
-    return
+	return
 }
 
 func VeriSign(uid, t, sign string) bool {
 
-    str := crypto.Rc4(sign, uid)
+	str := crypto.Rc4(sign, uid)
 
-    arr := strings.Split(str, "&")
-    if len(arr) < 2 {
-        return false
-    }
+	arr := strings.Split(str, "&")
+	if len(arr) < 2 {
+		return false
+	}
 
-    for _, item := range arr {
-        param := strings.Split(item, "=")
-        k := param[0]
-        v := param[1]
+	for _, item := range arr {
+		param := strings.Split(item, "=")
+		k := param[0]
+		v := param[1]
 
-        if k == "uid" && v != uid {
-            return false
-        }
+		if k == "uid" && v != uid {
+			return false
+		}
 
-        if k == "t" && v != t {
-            return false
-        }
-    }
+		if k == "t" && v != t {
+			return false
+		}
+	}
 
-    return true
+	return true
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/eavesmy/golang-lib
 go 1.14
 
 require (
+	github.com/fwhezfwhez/errorx v1.1.0
 	github.com/go-http-utils/cookie v1.3.1
 	github.com/google/uuid v1.2.0
 	github.com/satori/go.uuid v1.2.0
