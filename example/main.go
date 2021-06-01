4fe38b51fdeb3ecc45ae1a5a59c0d95b81734a92
jjc <1140147099@qq.com>
2021-06-01 14:32:00 +0800

    update token`s error

--- example/main.go
+++ example/main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"github.com/eavesmy/golang-lib/auth"
-)
-
 func main ()  {
 
 
-	token := auth.GenToken("u9822")
-	uid,t,sign := auth.ParseToken(token)
-
-	fmt.Println(uid,t,sign )
 
 }
