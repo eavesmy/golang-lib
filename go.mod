cadb18237d9509d3362ecb6d417726efecd6549b
eavesmy <eavesmy@gmail.com>
2022-01-04 11:03:13 +0800

    添加gbk2utf8方法

--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/google/uuid v1.2.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/text v0.3.7
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
---
+++ util/utf2gbk.go
@@ -0,0 +1,34 @@
+/*
+# File Name: utf2gbk.go
+# Author : eavesmy
+# Email:eavesmy@gmail.com
+# Created Time: 二  1/ 4 11:02:08 2022
+*/
+
+package util
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+)
+
+func GbkToUtf8(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+func Utf8ToGbk(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewEncoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
