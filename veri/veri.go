e3b7682a89d8081ceaf0ad22a154eefc735f6522
eavesmy <eavesmy@gmail.com>
2024-07-06 12:56:27 +0800

    添加190号码认证

--- veri/veri.go
+++ veri/veri.go
@@ -12,7 +12,7 @@ func IsEmailFormat(email string) bool {
 
 //mobile verify
 func IsMobileFormat(mobileNum string) bool {
-	regular := "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$"
+	regular := "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|190|199|(147))\\d{8}$"
 
 	reg := regexp.MustCompile(regular)
 	return reg.MatchString(mobileNum)
